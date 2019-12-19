bc8f2b5d5ebe0f906e71fc47a2eedae75f8b9b25
USUARIO 03 <USUARIO 03@DESKTOP-2BVO22Q>
2019-12-18 20:28:59 -0500

    Iniciop0' op0

---
+++ src/Practica/usaPractica.java
@@ -0,0 +1,12 @@
+
+package Practica;
+
+
+public class usaPractica {
+    public static void main(String[] args) {
+        Practica c = new Practica();
+        c.setVisible(true);
+        c.setLocationRelativeTo(null);
+    }
+   
+}
